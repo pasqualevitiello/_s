75f50f472222fa477c043625e2b610ac1f888ff1
pasqualevitiello <pasqualevitiello@gmail.com>
2019-12-17 16:04:00 +0100

    installing composer

---
+++ foo/greetings-traditional-way.php
@@ -0,0 +1,7 @@
+<?php
+class Greetings {
+
+    public function hello() {
+        return '<h1 style="color:blue;">An old fashioned Hello there!</h1>';
+    }
+}
---
+++ foo/greetings.php
@@ -0,0 +1,9 @@
+<?php
+namespace Foo;
+
+class Greetings {
+
+    public function hello() {
+        return '<h1 style="color:green;">A new Hello there!</h1>';
+    }
+}
